df6fff2b6f496e1ea4638c785a30d27de73fa21a
manoj <jmanojjmvr49@gmail.com>
2025-05-23 11:02:46 +0530

    text logo color changed

--- components/MobileSideBar.tsx
+++ components/MobileSideBar.tsx
@@ -1,46 +1,48 @@
-import { SidebarComponent } from "@syncfusion/ej2-react-navigations"
-import { Link } from "react-router"
+import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
+import { Link } from "react-router";
 import NavItems from "./NavItems";
 
 const MobileSideBar = () => {
-  
-  let sidebar:SidebarComponent;
-  const toggleSidebar = ()=>{
+  let sidebar: SidebarComponent;
+  const toggleSidebar = () => {
     sidebar.toggle();
-  }
-  
+  };
+
   return (
     <div className="mobile-sidebar wrapper">
       <header>
-        <Link to="/" >
-        <img src="assets/icons/logo.svg" alt="logo.svg " className="size-[30px]" />
-        <h1>Yatrika</h1>
+        <Link to="/">
+          <img
+            src="assets/icons/logo.svg"
+            alt="logo.svg "
+            className="size-[30px]"
+          />
+          <h1>
+           Yatri<span className="text-[#211dff]">k</span>a
+          </h1>
         </Link>
 
         {/* button to collaps the sidebar */}
         {/* @ts-ignore */}
         <button onClick={toggleSidebar}>
           <img src="/assets/icons/menu.svg" alt="menu" className="size-7" />
-
         </button>
       </header>
 
-      <SidebarComponent 
-      // @ts-ignore
-      width={270}
-      // @ts-ignore
-      ref={(Sidebar)=> sidebar = Sidebar}
-      created={()=>sidebar.hide()}
-      closeOnDocumentClick = {true}
-      showBackdrop={true}
-      type="over"
-
+      <SidebarComponent
+        // @ts-ignore
+        width={270}
+        // @ts-ignore
+        ref={(Sidebar) => (sidebar = Sidebar)}
+        created={() => sidebar.hide()}
+        closeOnDocumentClick={true}
+        showBackdrop={true}
+        type="over"
       >
         <NavItems handleClick={toggleSidebar} />
-        
-        </SidebarComponent> 
+      </SidebarComponent>
     </div>
-  )
-}
+  );
+};
 
-export default MobileSideBar
+export default MobileSideBar;
--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,54 +1,71 @@
-import { Link, NavLink } from 'react-router'
-import { sidebarItems } from '~/constants'
-import { cn } from '~/lib/utils'
+import { Link, NavLink } from "react-router";
+import { sidebarItems } from "~/constants";
+import { cn } from "~/lib/utils";
 
-const NavItems = ({handleClick}:{handleClick? :()=>void }) => {
-
+const NavItems = ({ handleClick }: { handleClick?: () => void }) => {
   const user = {
-    name : "manoj",
-    email : "jmanjjmvr@gmail.com",
-    imgUrl : "/assets/images/david.webp"
-  }
+    name: "manoj",
+    email: "jmanjjmvr@gmail.com",
+    imgUrl: "/assets/images/david.webp",
+  };
   return (
-    <section className='nav-items'>
-      <Link to='/' className='link-logo'>
-      <img src="/assets/icons/logo.svg" alt="logo" className='size-[30px]' />
-      <h1>Yatrika</h1>
-      </Link>  
+    <section className="nav-items">
+      <Link to="/" className="link-logo">
+        <img src="/assets/icons/logo.svg" alt="logo" className="size-[30px]" />
+        <h1>
+          Yatri<span className="text-[#211dff]">k</span>a
+        </h1>
+      </Link>
 
       <div className="container">
         <nav>
-          {sidebarItems.map(({id,href,label,icon})=>(
-            <NavLink to={href} key={id} >
-              {
-                ({isActive}:{isActive : boolean})=>
-                  (
-                    <div className={cn('group nav-item',{'bg-primary-100 !text-white':isActive})} onClick={handleClick}>
-                      <img src={icon} alt={label} className={`group-hover:brightness-0 size-5 group-hover:invert ${isActive ? 'brightness-0 invert' : 'text-dark-200'}`} />
-                      {label}
-                    </div>
-                  )
-              }
+          {sidebarItems.map(({ id, href, label, icon }) => (
+            <NavLink to={href} key={id}>
+              {({ isActive }: { isActive: boolean }) => (
+                <div
+                  className={cn("group nav-item", {
+                    "bg-primary-100 !text-white": isActive,
+                  })}
+                  onClick={handleClick}
+                >
+                  <img
+                    src={icon}
+                    alt={label}
+                    className={`group-hover:brightness-0 size-5 group-hover:invert ${
+                      isActive ? "brightness-0 invert" : "text-dark-200"
+                    }`}
+                  />
+                  {label}
+                </div>
+              )}
             </NavLink>
           ))}
         </nav>
-        <footer className='nav-footer'>
-            <img src={user?.imgUrl} alt={user?.imgUrl || "/assets/images/david.webp"} />
-            <article>
-              <h2>{user.name}</h2>
-              <p>{user.email}</p>
-            </article>
+        <footer className="nav-footer">
+          <img
+            src={user?.imgUrl}
+            alt={user?.imgUrl || "/assets/images/david.webp"}
+          />
+          <article>
+            <h2>{user.name}</h2>
+            <p>{user.email}</p>
+          </article>
 
-            <button onClick={()=>(
-              console.log("log out")
-            )} className='cursor-pointer'>
-
-              <img src="/assets/icons/logout.svg" alt="logout" className='size-8' title='logout' />
-            </button>
+          <button
+            onClick={() => console.log("log out")}
+            className="cursor-pointer"
+          >
+            <img
+              src="/assets/icons/logout.svg"
+              alt="logout"
+              className="size-8"
+              title="logout"
+            />
+          </button>
         </footer>
-      </div>  
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default NavItems
+export default NavItems;
